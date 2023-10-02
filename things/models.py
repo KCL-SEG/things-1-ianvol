cc9d6ced525fbf309683ad98d70b01b6c74c2434
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-10-02 12:50:21 +0100

    Extended model field

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=255, unique = True)
+    description = models.TextField(max_length=120, blank=True)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
