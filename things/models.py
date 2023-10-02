0075f02f6e69f3d138c20125e24dc440a9ff653d
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-10-02 13:02:36 +0100

    Extended model field v6

--- things/models.py
+++ things/models.py
@@ -3,5 +3,5 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique = True)
-    description = models.TextField(max_length=120, blank=True)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
